61dba53df90a6baff3ccc03d60788ccd76f564db
Abdur Rahim <mrrahim19710@gmail.com>
2022-06-30 01:53:02 +0600

    Laravel Home Services Project
    Laravel Home Services Project

--- HomeServices/resources/views/layouts/base.blade.php
+++ HomeServices/resources/views/layouts/base.blade.php
@@ -55,9 +55,11 @@
 
                 <ul class="collapse">
                     <li class="title">
-                        <a href="index.php.html"><img src="images/logo.png"></a>
+                        <a href="index.php.html"><img src="{{ asset('images/logo.png') }}"></a>
                     </li>
-                    <li> <a href="javascript:void(0);">Air Conditioners</a>
+                    <li> <a href="{{ route('home.service_categories') }}">Service Categories</a> </li>
+
+                   {{--  <li> <a href="javascript:void(0);">Air Conditioners</a>
                         <ul class="drop-down one-column hover-fade">
                             <li><a href="service-details/ac-wet-servicing.html">Wet Servicing</a></li>
                             <li><a href="service-details/ac-dry-servicing.html">Dry Servicing</a></li>
@@ -67,7 +69,7 @@
                             <li><a href="service-details/ac-gas-refill.html">Gas Refill</a></li>
                             <li><a href="service-details/ac-repair.html">Repair</a></li>
                         </ul>
-                    </li>
+                    </li> --}}
                     <li> <a href="#">Appliances</a>
                         <ul class="drop-down one-column hover-fade">
                             <li><a href="servicesbycategory/11.html">Computer Repair</a></li>
@@ -126,6 +128,7 @@
                         <li class="login-form"> <a href="#" title="Register">My Account (Admin)</a>
                             <ul class="drop-down one-column hover-fade">
                                 <li><a href="{{ route('admin.dashboard') }}">Dasboard</a></li>
+                                <li><a href="{{ route('admin.service_categories') }}">Service Categories</a></li>
                                 <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
                             </ul>
                         </li>
--- HomeServices/routes/web.php
+++ HomeServices/routes/web.php
@@ -1,22 +1,30 @@
 <?php
 
+use App\Http\Livewire\Admin\AdminAddServiceCategoryComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
+use App\Http\Livewire\Admin\AdminServiceCategoryComponent;
 use App\Http\Livewire\Customer\CustomerDashboardComponent;
 use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ServiceCategoriesComponent;
 use App\Http\Livewire\Sprovider\SproviderDashboardComponent;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeComponent::class)->name('home');
+Route::get('/service-categories', ServiceCategoriesComponent::class)->name('home.service_categories');
 
 ///For Customer
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('/customer/dashboard', CustomerDashboardComponent::class)->name('customer.dashboard');
 });
 
+///For Services Provider
 Route::middleware(['auth:sanctum', 'verified', 'authsprovider'])->group(function () {
     Route::get('/sprovider/dashboard', SproviderDashboardComponent::class)->name('sprovider.dashboard');
 });
 
+///For Admin
 Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function () {
     Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+    Route::get('/admin/service-categories', AdminServiceCategoryComponent::class)->name('admin.service_categories');
+    Route::get('/admin/service-category/add', AdminAddServiceCategoryComponent::class)->name('admin.add_service_category');
 });
